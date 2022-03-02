927b09236c3ea748cd01eac9af9a29e8f6fd2279
fabian <a129211@rec-dsk-lnx013.bf.arg.igrupobbva>
2022-03-02 17:39:25 -0300

    l

--- scripts/app-module.js
+++ scripts/app-module.js
@@ -1,5 +1,5 @@
-import '@webcomponents/shadycss/entrypoints/scoping-shim.js';
-import '@webcomponents/shadycss/entrypoints/custom-style-interface.js';
+//import '@webcomponents/shadycss/entrypoints/scoping-shim.js';
+//import '@webcomponents/shadycss/entrypoints/custom-style-interface.js';
 
 import '../vendor/cells/cells-polymer-bridge.min.js';
 import './lit-initial-components.js';
